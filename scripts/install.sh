193967e1560774316b95ea1a7473139d64be72a4
Iulian Pascalau <iulian.pascalau@gmail.com>
2021-01-17 03:21:54 +0200

    - added scripts

---
+++ scripts/install.sh
@@ -0,0 +1,22 @@
+echo "Installing systemd..."
+
+#Create the service file
+echo "[Unit]
+  Description=kaching-go
+  After=network-online.target
+
+  [Service]
+  User=ubuntu
+  WorkingDirectory=/home/ubuntu/kaching-go
+  ExecStart=/home/ubuntu/kaching-go/kaching-go &>> logfile.log
+  StandardOutput=journal
+  StandardError=journal
+  Restart=always
+  RestartSec=3
+  LimitNOFILE=4096
+
+  [Install]
+  WantedBy=multi-user.target" > kaching-go.service
+
+sudo mv kaching-go.service /etc/systemd/system/
+sudo systemctl enable kaching-go.service
---
+++ scripts/start.sh
@@ -0,0 +1,1 @@
+sudo systemctl start kaching-go
